2a08b6252529a585b30471d3ea7649c7e50885b6
RDR96 <00231514@uca.edu.sv>
2018-02-28 15:55:54 -0600

    Issue #2

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,10 +1,27 @@
 $(window).scroll(function()
 {
-  if( $("a.active") && !($("a.active").parents(".show").eq(2).length) ){
-    $("a.active").parents().eq(2).addClass("show");
-  }
-  if( !($("a.active").length) && $("a.active").parents(".show").eq(2) ){
-     $("a.active").parents().eq(2).removeClass("show");
+  
+  if( $(".show").find(".active").length == 0){
+     //$(".collapse").removeClass("show");
+     $(".collapse").collapse('hide');
   }
 
+  if( $("a.active") && !($("a.active").parents(".show").eq(2).length) ){
+    //$("a.active").parents().eq(2).addClass("show");
+    $("a.active").parents().eq(2).collapse('show');
+  }
+  
+
 });
+
+$("a.list-group-item").click(function()
+{
+  // alert("click");  
+    // $(this).parents(".collapse").eq(2).addClass("show");
+    if( $(this).parents(".collapse").eq(2).length == 0 ){
+      $(this).parents(".collapse").eq(2).collapse('show');
+    }
+    
+
+});
+
