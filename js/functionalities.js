b84d24f2e525242dbed76a36503e098f95643937
RDR96 <00231514@uca.edu.sv>
2018-02-28 12:39:02 -0600

    Issue #2

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,54 +1,10 @@
-
-$("a.nav-link ").click(function(e){
-
-  e.stopPropagation();
-
-  var className = this.getAttribute("class");
-
-  if( className == "nav-link ml-3 my-1"){
-
-    if($("a.active").attr("class") === "nav-link ml-3 my-1 active"){
-      $("a.active").attr("class","nav-link ml-3 my-1");
-    }else{
-        $("a.active").attr("class","nav-link font-weight-bold");
-    }
-
-    this.setAttribute("class","nav-link ml-3 my-1 active");
-
-  }else{
-
-    if($("a.active").attr("class") === "nav-link ml-3 my-1 active" ){
-      $("a.active").attr("class","nav-link ml-3 my-1");
-    }else{
-        $("a.active").attr("class","nav-link font-weight-bold");
-    }
-
-    this.setAttribute("class","nav-link font-weight-bold active");
+$(window).scroll(function()
+{
+  if( $("a.active") && !($("a.active").parents(".show").eq(2).length) ){
+    $("a.active").parents().eq(2).addClass("show");
   }
-});
-
-
-/*$(document).ready(function(){
-
-  $(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.back-to-top').slideDown("slow", function() {
-    // Animation complete.
-  });
-		} else {
-			$('.back-to-top').slideUp("slow", function() {
-    // Animation complete.
-  });
-		}
-	});
-
-	$('.back-to-top').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-
+  if( !($("a.active").length) && $("a.active").parents(".show").eq(2) ){
+     $("a.active").parents().eq(2).removeClass("show");
+  }
 
 });
-*/
