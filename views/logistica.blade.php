8fa504c77f34d5880a8a2560195f9d7f7d42086f
Mario Gallegos <mgallegos76@gmail.com>
2018-03-01 14:36:10 -0600

    Issue #6

--- views/logistica.blade.php
+++ views/logistica.blade.php
@@ -767,5 +767,19 @@
 
 </div>
 <!-- /.container -->
+<script>
+  $(window).scroll(function()
+  {    
+    if( $(".show").find(".active").length == 0){
+       //$(".collapse").removeClass("show");
+       $(".collapse").collapse('hide');
+    }
+
+    if( $("a.active") && !($("a.active").parents(".show").eq(2).length) ){
+      //$("a.active").parents().eq(2).addClass("show");
+      $("a.active").parents().eq(2).collapse('show');
+    }
+  });
+</script>
 @parent
 @stop
