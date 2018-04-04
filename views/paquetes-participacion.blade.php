4bf314ff6c8f1c9dd1e70a86567827e61eacc621
alvarogarcia1010 <00111317@uca.edu.sv>
2018-04-03 22:17:58 -0600

    Issue #11

--- views/paquetes-participacion.blade.php
+++ views/paquetes-participacion.blade.php
@@ -1,58 +1,52 @@
-
 <div class="card-deck">
 
-      <div class="card">
-        <h3 class="card-header text-center">Básico</h3>
-        <div class="card-body m-0 px-0">
-          <h2 class="text-center font-weight-bold">$ 10.00</h2>
-          <div class="font-italic text-center mb-4">Incluye:</div>
+  <!-- Paquete Básico -->
+  <div class="card">
+    <h3 class="card-header text-center">Básico</h3>
+    <div class="card-body m-0 px-0">
+      <h2 class="text-center font-weight-bold">$ 10.00</h2>
+      <div class="font-italic text-center mb-4">Incluye:</div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Ingreso al evento</li>
+        <li class="list-group-item">Promocional básico</li>
+        <li class="list-group-item border-bottom">Diploma de participación</li>
+      </ul>
+    </div>
+  </div>
 
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Ingreso al evento</li>
-          <li class="list-group-item">Promocional básico</li>
-          <li class="list-group-item border-bottom">Diploma de participación</li>
-        </ul>
-        </div>
-        </div>
+  <!-- Paquete Parcial -->
+  <div class="card">
+    <h3 class="card-header text-center">Parcial</h3>
+    <div class="card-body m-0 px-0">
+      <h2 class="text-center font-weight-bold">$ 40.00</h2>
+      <div class="font-italic text-center mb-4">Incluye:</div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Ingreso al evento</li>
+        <li class="list-group-item">Promocional Completo</li>
+        <li class="list-group-item">Diploma de participación</li>
+        <li class="list-group-item border-bottom">Almuerzos</li>
+      </ul>
+    </div>
+  </div>
 
+  <!-- Paquete Completo -->
+  <div class="card ">
+    <h3 class="card-header text-center">Completo</h3>
+    <div class="card-body m-0 px-0">
+      <h2 class="text-center font-weight-bold">$ 150.00</h2>
+      <div class="font-italic text-center">Incluye:</div>
+    </div>
+    <ul class="list-group list-group-flush" >
+      <li class="list-group-item">Ingreso al evento</li>
+      <li class="list-group-item">Promocional Completo</li>
+      <li class="list-group-item">Diploma de participación</li>
+      <li class="list-group-item">Comida (3 tiempos)</li>
+      <li class="list-group-item">Hospedaje (3 noches)</li>
+      <li class="list-group-item">Transporte</li>
+    </ul>
+  </div>
 
-
-
-      <div class="card">
-        <h3 class="card-header text-center">Parcial</h3>
-        <div class="card-body m-0 px-0">
-          <h2 class="text-center font-weight-bold">$ 40.00</h2>
-          <div class="font-italic text-center mb-4">Incluye:</div>
-
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Ingreso al evento</li>
-          <li class="list-group-item">Promocional Completo</li>
-          <li class="list-group-item">Diploma de participación</li>
-          <li class="list-group-item border-bottom">Almuerzos</li>
-
-        </ul>
-        </div>
-        </div>
-
-
-
-
-      <div class="card ">
-        <h3 class="card-header text-center">Completo</h3>
-        <div class="card-body m-0 px-0">
-          <h2 class="text-center font-weight-bold">$ 150.00</h2>
-          <div class="font-italic text-center">Incluye:</div>
-        </div>
-        <ul class="list-group list-group-flush" >
-          <li class="list-group-item">Ingreso al evento</li>
-          <li class="list-group-item">Promocional Completo</li>
-          <li class="list-group-item">Diploma de participación</li>
-          <li class="list-group-item">Comida (3 tiempos)</li>
-          <li class="list-group-item">Hospedaje (3 noches)</li>
-          <li class="list-group-item">Transporte</li>
-        </ul>
-        </div>
-      </div>
+</div>
 
 
 
