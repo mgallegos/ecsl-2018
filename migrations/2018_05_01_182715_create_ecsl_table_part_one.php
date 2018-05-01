6d7161280f6456b24ee02dd4a5f0b87f27b627a3
Mario Gallegos <mgallegos76@gmail.com>
2018-05-01 17:55:25 -0600

    Issue #7

---
+++ migrations/2018_05_01_182715_create_ecsl_table_part_one.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEcslTablePartOne extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('ECSL_Registration_Form', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('contact_phone', 60)->nullable();
+        $table->string('emergency_contact_name', 100)->nullable();
+        $table->string('emergency_contact_phone', 60)->nullable();
+        $table->string('passport', 60)->nullable();
+        $table->string('country', 60)->nullable();
+        $table->string('shirt_size', 10)->nullable();
+        $table->date('birth_date')->nullable();
+        $table->string('gender', 60)->nullable();
+        $table->string('custom_gender', 60)->nullable();
+        $table->string('institution', 100)->nullable();
+        $table->text('health_condition')->nullable();
+        $table->text('specific_needs')->nullable();
+        $table->boolean('participated_in_ecsl2009')->default(false);
+        $table->boolean('participated_in_ecsl2010')->default(false);
+        $table->boolean('participated_in_ecsl2011')->default(false);
+        $table->boolean('participated_in_ecsl2012')->default(false);
+        $table->boolean('participated_in_ecsl2013')->default(false);
+        $table->boolean('participated_in_ecsl2014')->default(false);
+        $table->boolean('participated_in_ecsl2015')->default(false);
+        $table->boolean('participated_in_ecsl2016')->default(false);
+        $table->boolean('participated_in_ecsl2017')->default(false);
+        $table->boolean('is_debian_linux_user')->default(false);
+        $table->boolean('is_ubuntu_linux_user')->default(false);
+        $table->boolean('is_mint_linux_user')->default(false);
+        $table->boolean('is_arch_linux_user')->default(false);
+        $table->boolean('is_slackware_linux_user')->default(false);
+        $table->boolean('is_opensuse_linux_user')->default(false);
+        $table->boolean('is_knoppix_linux_user')->default(false);
+        $table->boolean('is_centos_linux_user')->default(false);
+        $table->boolean('is_elementary_linux_user')->default(false);
+        $table->boolean('is_damn_linux_user')->default(false);
+        $table->boolean('is_dream_linux_user')->default(false);
+        $table->boolean('is_fedora_linux_user')->default(false);
+        $table->boolean('is_gentoo_linux_user')->default(false);
+        $table->boolean('is_antergos_linux_user')->default(false);
+        $table->string('custom_distribution', 60)->nullable();
+        $table->boolean('is_vegetarian')->default(false);
+        $table->boolean('is_interested_in_competition')->default(false);
+        $table->boolean('is_gender_visible')->default(false);
+        $table->boolean('is_general_information_visible')->default(false);
+        $table->boolean('is_photo_visible')->default(false);
+
+        $table->unsignedInteger('arriving_transportation_request_id')->index();
+        $table->foreign('arriving_transportation_request_id')->references('id')->on('OCMS_Transportation_Request');
+        $table->unsignedInteger('leaving_transportation_request_id')->index();
+        $table->foreign('leaving_transportation_request_id')->references('id')->on('OCMS_Transportation_Request');
+        $table->unsignedInteger('user_id')->index();
+        $table->foreign('user_id')->references('id')->on('OCMS_User');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ECSL_Registration');
+    }
+}
