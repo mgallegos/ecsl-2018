723e231f6f272a55160f6de5bb31ce7aa1220626
Mario Gallegos <mgallegos76@gmail.com>
2018-07-13 10:38:11 -0600

    Issue #17

--- Mgallegos/ECSL2018/Services/OpenCmsManagement/Ecsl2018OpenCmsManager.php
+++ Mgallegos/ECSL2018/Services/OpenCmsManagement/Ecsl2018OpenCmsManager.php
@@ -2478,6 +2478,7 @@ class Ecsl2018OpenCmsManager extends OpenCmsManager {
    */
   public function saoh03(array $input)
   {
+    var_dump($input);die();
 		$Organization = $this->AuthenticationManager->getOrganizationByApiToken($input['token']);
 
     if(empty($Organization))
